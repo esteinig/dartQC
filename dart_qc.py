81737d1873b6803603924c30e97305633ef6c2a2
Eike J. Steinig <eikejoachim.steinig@my.jcu.edu.au>
2016-04-12 14:00:38 +1000

    Error fixes

--- dart_qc.py
+++ dart_qc.py
@@ -166,7 +166,7 @@ class CommandLine:
         self.parser = argparse.ArgumentParser(description='DartQC Pipeline v.0.1', add_help=True)
         self.setParser()
 
-        self.args = self.parser.parse_args()
+        self.args = self.parser.parse_args(['-c', 'config_test.csv'])
         self.arg_dict = vars(self.args)
 
     def setParser(self):
