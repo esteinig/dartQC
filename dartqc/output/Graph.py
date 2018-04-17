7c99ddcca6a456074d8db23cc6760901afafcb88
Casey Bajema <casey.bajema1@jcu.edu.au>
2018-04-17 14:31:42 +1000

    Changed graph output type to .png as .jpg requires specific libraries (pillow) installed in python...

--- dartqc/output/Graph.py
+++ dartqc/output/Graph.py
@@ -15,6 +15,8 @@ log = logging.getLogger(__file__)
 
 
 class GraphOutput(Output):
+    GRAPH_IMG_TYPE = ".png"
+
     def get_name(self) -> str:
         return "graph"
 
@@ -32,14 +34,14 @@ class GraphOutput(Output):
         for allele_id in dataset.filtered.snps:
             del filtered_read_counts[allele_id]
 
-        GraphTypes.read_counts_per_individ([filtered_read_counts], colors=["b"]).to_file(os.path.join(folder, filter_name +"_IndividReadCounts.jpg"))
-        GraphTypes.avg_reads_per_snp([filtered_read_counts], colors=["b"]).to_file(os.path.join(folder,filter_name + "_AvgReadsPerSNP.jpg"))
-        GraphTypes.avg_rep_across_snp([filtered_snps], colors=["b"]).to_file(os.path.join(folder,filter_name + "_AvgRepAcrossSNP.jpg"))
-        GraphTypes.het_across_snp([filtered_data], colors=["b"]).to_file(os.path.join(folder,filter_name + "_HetAcrossSNP.jpg"))
-        GraphTypes.call_rates_across_snp([filtered_data], colors=["b"]).to_file(os.path.join(folder,filter_name + "_CallRatesAcrossSNP.jpg"))
-        GraphTypes.call_rate_across_individ([filtered_data], colors=["b"]).to_file(os.path.join(folder,filter_name + "_CallRatesAcrossIndivid.jpg"))
-        GraphTypes.maf_across_snp([maf], colors=["b"]).to_file(os.path.join(folder,filter_name + "_MAFAcrossSNP.jpg"))
-        GraphTypes.maf_to_read_count([maf], [filtered_read_counts], colors=["b"]).to_file(os.path.join(folder,filter_name + "_MAFToReadCount.jpg"))
-        GraphTypes.call_rate_to_maf([filtered_data], [maf], colors=["b"]).to_file(os.path.join(folder, filter_name + "_CallRateToMAF.jpg"))
+        GraphTypes.read_counts_per_individ([filtered_read_counts], colors=["b"]).to_file(os.path.join(folder, filter_name +"_IndividReadCounts" + self.GRAPH_IMG_TYPE))
+        GraphTypes.avg_reads_per_snp([filtered_read_counts], colors=["b"]).to_file(os.path.join(folder,filter_name + "_AvgReadsPerSNP" + self.GRAPH_IMG_TYPE))
+        GraphTypes.avg_rep_across_snp([filtered_snps], colors=["b"]).to_file(os.path.join(folder,filter_name + "_AvgRepAcrossSNP" + self.GRAPH_IMG_TYPE))
+        GraphTypes.het_across_snp([filtered_data], colors=["b"]).to_file(os.path.join(folder,filter_name + "_HetAcrossSNP" + self.GRAPH_IMG_TYPE))
+        GraphTypes.call_rates_across_snp([filtered_data], colors=["b"]).to_file(os.path.join(folder,filter_name + "_CallRatesAcrossSNP" + self.GRAPH_IMG_TYPE))
+        GraphTypes.call_rate_across_individ([filtered_data], colors=["b"]).to_file(os.path.join(folder,filter_name + "_CallRatesAcrossIndivid" + self.GRAPH_IMG_TYPE))
+        GraphTypes.maf_across_snp([maf], colors=["b"]).to_file(os.path.join(folder,filter_name + "_MAFAcrossSNP" + self.GRAPH_IMG_TYPE))
+        GraphTypes.maf_to_read_count([maf], [filtered_read_counts], colors=["b"]).to_file(os.path.join(folder,filter_name + "_MAFToReadCount" + self.GRAPH_IMG_TYPE))
+        GraphTypes.call_rate_to_maf([filtered_data], [maf], colors=["b"]).to_file(os.path.join(folder, filter_name + "_CallRateToMAF" + self.GRAPH_IMG_TYPE))
 
 GraphOutput()
